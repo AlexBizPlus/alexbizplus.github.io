19a59b78e472f7bb31864764238e11a4cc53bae4
AlexBizPlus <alexeynejnov@gmail.com>
2020-08-02 14:01:39 +0700

    Fix list 4

--- js/slider-mobile.js
+++ js/slider-mobile.js
@@ -1,95 +1,99 @@
-  (function () {
-    var sliderList = document.getElementById('slider-list');
-    var sliderLink = document.getElementById('slider-link');
-    var elements = sliderList.getElementsByTagName('input');
-    var getCurrentIndex = function (index) {
-      return index = Array.from(elements).findIndex(function (elementArray) {
-        return elementArray.checked;
-      });
-    };
-
-    var currentElement = getCurrentIndex(currentElement);
-
-    sliderLink.addEventListener('mousedown', function (evt) {
-      evt.preventDefault();
-      var startCoordX = evt.clientX;
-      var shift;
-      var dragged = false;
-
-      var onMouseMove = function (moveEvt) {
-        moveEvt.preventDefault();
-        dragged = true;
-        shift = startCoordX - moveEvt.clientX;
-      };
-
-      var onMouseUp = function (upEvt) {
-        upEvt.preventDefault();
-
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-
-        if (dragged) {
-          var onClickPreventDefault = function (clickEvt) {
-            clickEvt.preventDefault();
-            sliderLink.removeEventListener('click', onClickPreventDefault);
-          };
-
-          sliderLink.classList.remove(`slider__link--${elements[currentElement].id}`);
-          elements[currentElement].checked = false;
-
-          if (shift < -2) {
-            currentElement = (currentElement >= 1) ? --currentElement : elements.length - 1;
-          }
-
-          if (shift > 2) {
-            currentElement = (currentElement < elements.length - 1) ? ++currentElement : 0;
-          }
-          sliderLink.classList.add(`slider__link--${elements[currentElement].id}`);
-          sliderLink.href = elements[currentElement].value;
-          elements[currentElement].checked = true;
-
-          sliderLink.addEventListener('click', onClickPreventDefault);
-        }
-
-        if (!dragged) {
-          var onSliderLinkClick = function () {
-            for (var i = 0; i < elements.length; i++) {
-              elements[i].checked = false;
-            }
-            elements[0].checked = true;
-            currentElement = 0;
-            sliderLink.removeEventListener('click', onClickPreventDefault);
-          };
-
-          sliderLink.addEventListener('click', onSliderLinkClick);
-        }
-      };
-
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
+(function () {
+  var sliderList = document.getElementById('slider-list');
+  var sliderLink = document.getElementById('slider-link');
+  var elements = sliderList.getElementsByTagName('input');
+  var getCurrentIndex = function (index) {
+    return index = Array.from(elements).findIndex(function (elementArray) {
+      return elementArray.checked;
     });
+  };
 
-    var onSliderListClick = function (evt) {
-      for (var i = 0; i < elements.length; i++) {
-        if (evt.target === elements[i] && evt.target !== elements[currentElement]) {
+  var currentElement = getCurrentIndex(currentElement);
 
-          sliderLink.classList.remove(`slider__link--${elements[currentElement].id}`);
-          elements[currentElement].checked = false;
+  sliderLink.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoordX = evt.clientX;
+    var shift;
+    var dragged = false;
 
-          sliderLink.classList.add(`slider__link--${evt.target.id}`);
-          sliderLink.href = evt.target.value;
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+      shift = startCoordX - moveEvt.clientX;
+    };
 
-          currentElement = evt.target.id;
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          sliderLink.removeEventListener('click', onClickPreventDefault);
+        };
+
+        sliderLink.classList.remove(`slider__link--${elements[currentElement].id}`);
+        elements[currentElement].checked = false;
+
+        if (shift < -2) {
+          currentElement = (currentElement >= 1) ? --currentElement : elements.length - 1;
         }
+
+        if (shift > 2) {
+          currentElement = (currentElement < elements.length - 1) ? ++currentElement : 0;
+        }
+        sliderLink.classList.add(`slider__link--${elements[currentElement].id}`);
+        sliderLink.href = elements[currentElement].value;
+        elements[currentElement].checked = true;
+
+        sliderLink.addEventListener('click', onClickPreventDefault);
+      }
+
+      if (!dragged) {
+        var onSliderLinkClick = function () {
+          for (var i = 0; i < elements.length; i++) {
+            elements[i].checked = false;
+          }
+          elements[0].checked = true;
+          currentElement = 0;
+          sliderLink.removeEventListener('click', onClickPreventDefault);
+        };
+
+        sliderLink.addEventListener('click', onSliderLinkClick);
       }
     };
-    sliderList.addEventListener('click', onSliderListClick);
 
-    window.addEventListener('DOMContentLoaded', function () {
-      for (var i = 0; i < elements.length; i++) {
-        elements[i].checked = false;
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  var onSliderListClick = function (evt) {
+    for (var i = 0; i < elements.length; i++) {
+      if (evt.target === elements[i] && evt.target !== elements[currentElement]) {
+
+        sliderLink.classList.remove(`slider__link--${elements[currentElement].id}`);
+        elements[currentElement].checked = false;
+
+        sliderLink.classList.add(`slider__link--${evt.target.id}`);
+        sliderLink.href = evt.target.value;
+
+        currentElement = evt.target.id;
       }
-      elements[0].checked = true;
-      currentElement = 0;
-    })
-  })();
+    }
+  };
+  sliderList.addEventListener('click', onSliderListClick);
+
+  var onSliderLinkClick = function () {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].checked = false;
+    }
+    elements[0].checked = true;
+    currentElement = 0;
+    sliderLink.removeEventListener('click', onClickPreventDefault);
+  };
+
+  sliderLink.addEventListener('click', onSliderLinkClick);
+
+})();
